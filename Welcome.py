3a10f34fd0fc3868183b4509f8e5afb10decece0
Anastasiia Alyoshkina <allenphos@gmail.com>
2024-11-25 17:10:40 +0100

    Some changes

--- Welcome.py
+++ Welcome.py
@@ -1,16 +1,24 @@
 # Welcome message
-print (" Welcome , everyone , to the GitHub tutorial !")
+print(" Welcome , everyone , to the GitHub tutorial !")
 # Brief task : define a simple function and call it
-def introduction_task () :
-    print ("\n- - - Python Task ---")
-    print ("Let ’s start by writing a function that adds two numbers !")
+
+
+def introduction_task():
+    print("\n- - - Python Task ---")
+    print("Let ’s start by writing a function that adds two numbers !")
 # Sample function
-def add_numbers (a , b ) :
+
+
+def add_numbers(a, b):
     return a + b
+
+
 # Task : Use the function with example values
 num1 = 5
 num2 = 7
-result = add_numbers ( num1 , num2 )
-print ( f"The result of adding { num1 } and { num2 } is: { result }")
+result = add_numbers(num1, num2)
+print(f"The result of adding { num1 } and { num2 } is: { result }")
 # Run the task
-introduction_task ()
+introduction_task()
+
+print("Some changes")
